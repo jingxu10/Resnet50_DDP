feac9bfade6b146e689a1a547b088ede42bafae0
Jing Xu <jing.xu@intel.com>
2021-09-19 17:37:49 +0900

    fine tune

--- resnet_ddp.py
+++ resnet_ddp.py
@@ -18,72 +18,63 @@ except:
     pass
 
 EPOCH = 100
-BATCH_SIZE = 256
 LR = 0.001
 DOWNLOAD = True
 DATA = 'datasets/cifar10/'
 
-def train(train_loader, net, criterion, optimizer, epoch, device, rank, world_size):
+def train(train_loader, net, criterion, optimizer, epoch, args):
     if is_hvd_enabled:
         hvd.broadcast_parameters(net.state_dict(), root_rank=0)
         hvd.broadcast_optimizer_state(optimizer, root_rank=0)
     net.train()
     t0 = time.time()
     for batch_idx, (data, target) in enumerate(train_loader):
-        data = data.to(device)
-        target = target.to(device)
+        data = data.to(args.device, non_blocking=True)
+        target = target.to(args.device, non_blocking=True)
         optimizer.zero_grad()
         output = net(data)
         loss = criterion(output, target)
         loss.backward()
         optimizer.step()
-        if batch_idx % 10 == 0 or len(data) < BATCH_SIZE:
-            print('[{}] Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(rank, epoch, world_size * batch_idx * len(data), len(train_loader.dataset), 100. * batch_idx / len(train_loader), loss.item()))
+        if batch_idx % 10 == 0 or len(data) < args.batch_size:
+            print('[{}] Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(args.rank, epoch, args.world_size * batch_idx * len(data), len(train_loader.dataset), 100. * batch_idx / len(train_loader), loss.item()))
     t1 = time.time()
     print('time elapsed: {:.2f}s'.format(t1-t0))
 
-def test(test_loader, net, criterion, optimizer, device, rank):
+def test(test_loader, net, criterion, optimizer, args):
     net.eval()
     test_loss = 0
     correct = 0
     count = 0
     with torch.no_grad():
         for data, target in test_loader:
-            data = data.to(device)
-            target = target.to(device)
+            data = data.to(args.device, non_blocking=True)
+            target = target.to(args.device, non_blocking=True)
             output = net(data)
             test_loss += criterion(output, target).item() * len(data) # sum up batch loss
             pred = output.argmax(dim=1, keepdim=True) # get the index of the max log-probability
             correct += pred.eq(target.view_as(pred)).sum().item()
             count += len(data)
     test_loss /= count
-    print('[{}] Test set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)'.format(rank, test_loss, correct, count, 100. * correct / count))
+    print('[{}] Test set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)'.format(args.rank, test_loss, correct, count, 100. * correct / count))
     return test_loss
 
-def main(local_rank, rank, world_size, backend='gloo', master_addr='127.0.0.1', port='29500'):
-    device = 'cpu'
-    if backend == 'nccl':
-        device = 'cuda:{}'.format(local_rank)
-    print('Device: {}'.format(device))
-    print('Backend: {}'.format(backend))
-
+def main(args):
     torch.manual_seed(10)
 
-    if world_size > 1:
-        if device.startswith('cuda'):
-            torch.cuda.set_device(local_rank)
+    if args.world_size > 1:
         if is_hvd_enabled:
             print('Distributed training with Horovod')
         else:
             print('Distributed training with DDP')
             if not 'MASTER_ADDR' in os.environ:
-                os.environ['MASTER_ADDR'] = master_addr
+                os.environ['MASTER_ADDR'] = args.master_addr
             if not 'MASTER_PORT' in os.environ:
-                os.environ['MASTER_PORT'] = port
-            os.environ['RANK'] = str(rank)
-            os.environ['WORLD_SIZE'] = str(world_size)
+                os.environ['MASTER_PORT'] = args.port
+            os.environ['RANK'] = str(args.rank)
+            os.environ['WORLD_SIZE'] = str(args.world_size)
             dist.init_process_group(
-                    backend=backend,
+                    backend=args.backend,
                     init_method='env://'
             )
 
@@ -110,7 +101,7 @@ def main(local_rank, rank, world_size, backend='gloo', master_addr='127.0.0.1', 
             sampler_train = torch.utils.data.distributed.DistributedSampler(train_dataset)
     train_loader = Data.DataLoader(
             dataset=train_dataset,
-            batch_size=BATCH_SIZE,
+            batch_size=args.batch_size,
             sampler=sampler_train
     )
     # test_dataset = torchvision.datasets.ImageFolder(
@@ -131,15 +122,17 @@ def main(local_rank, rank, world_size, backend='gloo', master_addr='127.0.0.1', 
             sampler_test = torch.utils.data.distributed.DistributedSampler(test_dataset)
     test_loader = Data.DataLoader(
             dataset=test_dataset,
-            batch_size=BATCH_SIZE,
+            batch_size=args.batch_size,
             sampler=sampler_test
     )
 
     net = torchvision.models.resnet50()
-    net.to(device)
-    if world_size > 1 and not is_hvd_enabled:
-        net = torch.nn.parallel.DistributedDataParallel(net)
+    net = net.to(args.device)
+    if args.world_size > 1 and not is_hvd_enabled:
+        device_ids = None
+        net = torch.nn.parallel.DistributedDataParallel(net, device_ids=device_ids)
     criterion = nn.CrossEntropyLoss()
+    criterion = criterion.to(args.device)
     lr_scaler = 1
     if is_hvd_enabled:
         lr_scaler = hvd.size()
@@ -148,14 +141,14 @@ def main(local_rank, rank, world_size, backend='gloo', master_addr='127.0.0.1', 
         optimizer = hvd.DistributedOptimizer(optimizer, named_parameters=net.named_parameters())
 
     for epoch in range(EPOCH):
-        train(train_loader, net, criterion, optimizer, epoch, device, rank, world_size)
+        train(train_loader, net, criterion, optimizer, epoch, args)
         if rank == 0 and (epoch + 1) % 10 == 0:
             torch.save({
                 'epoch': epoch,
                 'model_state_dict': net.state_dict(),
                 'optimizer_state_dict': optimizer.state_dict(),
                 }, 'checkpoint_{}.pth'.format(epoch))
-        loss = test(test_loader, net, criterion, optimizer, device, rank)
+        loss = test(test_loader, net, criterion, optimizer, args)
         if loss <= 0.000001:
             break
 
@@ -165,17 +158,17 @@ if __name__ == '__main__':
     parser.add_argument('--rank', default=0, type=int, help='rank')
     parser.add_argument('--local_world_size', default=1, type=int, help='local world size')
     parser.add_argument('--world_size', default=1, type=int, help='world size')
+    parser.add_argument('--device', default='cpu', type=str, help='Device to run on, default to cpu')
     parser.add_argument('--backend', default='gloo', type=str, help='DDP backend, default to gloo')
     parser.add_argument('--master_addr', default='127.0.0.1', type=str, help='Master Addr')
     parser.add_argument('--port', default='29500', type=str, help='Port')
+    parser.add_argument('--batch_size', default=256, type=int, help='Batch size')
     args = parser.parse_args()
 
     local_rank = args.local_rank
     rank = args.rank
     local_world_size = args.local_world_size
     world_size = args.world_size
-    if args.backend == 'ccl':
-        import torch_ccl
     if is_hvd_enabled:
         hvd.init()
         if hvd.size() > 1:
@@ -200,5 +193,20 @@ if __name__ == '__main__':
             rank = int(os.environ.get('RANK', args.rank))
             local_world_size = int(os.environ.get('LOCAL_WORLD_SIZE', args.local_world_size))
             world_size = int(os.environ.get('WORLD_SIZE', args.world_size))
+    args.local_rank = local_rank
+    args.rank = rank
+    args.local_world_size = local_world_size
+    args.world_size = world_size
     print('local | global: {}/{} | {}/{}'.format(local_rank, local_world_size, rank, world_size))
-    main(local_rank, rank, world_size, args.backend, args.master_addr, args.port)
+
+    if args.device == 'cuda':
+        args.backend = 'nccl'
+        if args.local_world_size > 0:
+            args.device = 'cuda:{}'.format(args.local_rank)
+            # torch.cuda.set_device(args.local_rank)
+    if args.backend == 'ccl':
+        import torch_ccl
+    print('Device: {}'.format(args.device))
+    print('Backend: {}'.format(args.backend))
+
+    main(args)
